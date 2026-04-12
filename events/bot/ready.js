65ec069589727647faad083b6a7ae126979db9cf
MsVoxxie <odysseylove222@gmail.com>
2026-04-12 04:59:04 -0400

    Fix optional chaining for linksFixed count and add ecosystem configuration for process management

--- events/bot/ready.js
+++ events/bot/ready.js
@@ -12,7 +12,7 @@ module.exports = {
 
 		// Get the number of links fixed
 		const linksFixed = await fixedLinks.findOne();
-		const linksFixedCount = linksFixed.linksFixed.toLocaleString();
+		const linksFixedCount = linksFixed?.linksFixed?.toLocaleString();
 
 		// Set the bot's activity
 		await client.user.setActivity(`I've Fixed ${linksFixedCount} links.`, { type: ActivityType.Custom });
